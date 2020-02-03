0faaf4e56459c3e3b967d6586f4825b3babe46e7
chenxinyu <chenxinyu@itv.baidu.com>
2020-02-03 17:26:36 +0800

    27000

--- code/test.py
+++ code/test.py
@@ -1,15 +1,22 @@
-import json
+import numpy as np
 import pandas
 import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
-config = json.load(open('./config.json', 'r'))
-print(config)
+data = pandas.read_csv('./data/data_lon_lat/1/video_3.csv')
+data = np.array(data)
 
-for uid in range(1, 11):
-    data = pandas.read_csv('./data/data_lon_lat/' + str(uid) + '/video_3.csv', usecols=[1])
-    plt.plot(data)
+lat = data[:,1:2]
+
+# print(lat.shape)
+
+ecdf = sm.distributions.ECDF(lat)
+x = np.linspace(0, 360)
+y = ecdf(x)
+plt.ylim((0, 1.0))
+plt.plot(x, y)
+plt.xlabel('xxx')
+plt.ylabel('yyy')
+# plt.title('ECDF ' + params_name, fontdict=font)
+plt.legend(loc='upper right', shadow=True)
 plt.show()
-
-# data = pandas.read_csv('./data/data_lon_lat/10/video_3.csv', usecols=[1])
-# plt.plot(data)
-# plt.show()
